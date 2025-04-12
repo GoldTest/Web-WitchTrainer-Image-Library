f1001750dbdd33ee421899cd6c9e5bef78943b4a
试金石 <1304747347@qq.com>
2025-04-12 12:58:01 +0800

    feature

--- webpage/defaults.js
+++ webpage/defaults.js
@@ -1,141 +1,139 @@
 'use strict';
 
 var Defaults = {
-  'hermione': 'backdrops:bathroom;' +
-    'shirts:tits.pressed/sports;' +
-    'pants:straight_jeans_belt;' +
-    'arms_right:default;' +
-    'arms_left:default;' +
-    'eyes:brown_default;' +
-    'hair:wavy_brown_pink_highlights;' +
-    'head:default;' +
-    'legs:default;' +
-    'mouth:nude_default;' +
-    'tits:default;' +
-    'torso:default;' +
-    'genitalia:default;' +
-    'hair:stylish_blonde;',
+    'hermione': 'backdrops:bathroom;' +
+        'arms_right:default;' +
+        'arms_left:default;' +
+        'eyes:brown_default;' +
+        'hair:wavy_brown_pink_highlights;' +
+        'head:default;' +
+        'legs:default;' +
+        'mouth:nude_default;' +
+        'tits:default;' +
+        'torso:default;' +
+        'onepieces:tits.pressed/gala;' +
+        'genitalia:default;',
 
-  'cho': 'backdrops:bathroom;' +
-    'arms_left:default;' +
-    'arms_right:default;' +
-    'eyes:brown_default;' +
-    'hair:default;' +
-    'head:default;' +
-    'legs:default;' +
-    'mouth:nude_default;' +
-    'tits:default;' +
-    'torso:default;' +
-    'genitalia:default;' +
-    'skirts:high_long;' +
-    'shirts:tits.default/vest_r;',
+    'cho': 'backdrops:bathroom;' +
+        'arms_left:default;' +
+        'arms_right:default;' +
+        'eyes:brown_default;' +
+        'hair:default;' +
+        'head:default;' +
+        'legs:default;' +
+        'mouth:nude_default;' +
+        'tits:default;' +
+        'torso:default;' +
+        'genitalia:default;' +
+        'skirts:high_long;' +
+        'shirts:tits.default/vest_r;',
 
-  'daphne': 'backdrops:bathroom;' +
-    'shirts:tits.default/uniform;' +
-    'skirts:uniform;' +
-    'arms_right:default;' +
-    'arms_left:default;' +
-    'cheeks:default;' +
-    'eyes:blue_default;' +
-    'hair2:blonde_shoulder;' +
-    'head:default;' +
-    'mouth:nude_default;' +
-    'tits:default;' +
-    'torso:default;' +
-    'legs:default;' +
-    'head:default;',
+    'daphne': 'backdrops:bathroom;' +
+        'shirts:tits.default/uniform;' +
+        'skirts:uniform;' +
+        'arms_right:default;' +
+        'arms_left:default;' +
+        'cheeks:default;' +
+        'eyes:blue_default;' +
+        'hair2:blonde_shoulder;' +
+        'head:default;' +
+        'mouth:nude_default;' +
+        'tits:default;' +
+        'torso:default;' +
+        'legs:default;' +
+        'head:default;',
 
-  'genie': 'backdrops:bathroom;' +
-    'pants:default;' +
-    'arms_right:default;' +
-    'arms_left:default;' +
-    'head:default;' +
-    'legs:default;' +
-    'pubic_hair:default;torso:default;' +
-    'genitalia:default;',
+    'genie': 'backdrops:bathroom;' +
+        'pants:default;' +
+        'arms_right:default;' +
+        'arms_left:default;' +
+        'head:default;' +
+        'legs:default;' +
+        'pubic_hair:default;torso:default;' +
+        'genitalia:default;',
 
-  'ginny': 'backdrops:bathroom;' +
-    'shirts:tits.pressed/vest_g;' +
-    'skirts:high_long;' +
-    'arms_right:default;' +
-    'arms_left:default;' +
-    'eyes:blue_default;' +
-    'hair2:default;' +
-    'layerhair:default;' +
-    'head:default;' +
-    'legs:default;' +
-    'mouth:nude_default;' +
-    'tits:default;' +
-    'torso:default;' +
-    'genitalia:default;',
+    'ginny': 'backdrops:bathroom;' +
+        'shirts:tits.pressed/vest_g;' +
+        'skirts:high_long;' +
+        'arms_right:default;' +
+        'arms_left:default;' +
+        'eyes:blue_default;' +
+        'hair2:default;' +
+        'layerhair:default;' +
+        'head:default;' +
+        'legs:default;' +
+        'mouth:nude_default;' +
+        'tits:default;' +
+        'torso:default;' +
+        'genitalia:default;',
 
-  'jasmine': 'backdrops:bathroom;' +
-    'shirts:default;' +
-    'pants:default;' +
-    'headgear:veil;' +
-    'arms_right:default;' +
-    'arms_left:default;' +
-    'hair:default;' +
-    'head:default;' +
-    'legs:default;' +
-    'eyes:default;' +
-    'mouth:default;' +
-    'tits:default;' +
-    'torso:default;',
+    'jasmine': 'backdrops:bathroom;' +
+        'shirts:default;' +
+        'pants:default;' +
+        'headgear:veil;' +
+        'arms_right:default;' +
+        'arms_left:default;' +
+        'hair:default;' +
+        'head:default;' +
+        'legs:default;' +
+        'eyes:default;' +
+        'mouth:default;' +
+        'tits:default;' +
+        'torso:default;',
 
-  'lola': 'backdrops:bathroom;' +
-    'shirts:default_long;' +
-    'skirts:default;' +
-    'arms_left:default;' +
-    'arms_right:default;' +
-    'hair:default;' +
-    'head:default;' +
-    'legs:default;' +
-    'eyes:default;' +
-    'mouth:default;' +
-    'tits:default;' +
-    'torso:default;',
+    'lola': 'backdrops:bathroom;' +
+        'shirts:default_long;' +
+        'skirts:default;' +
+        'arms_left:default;' +
+        'arms_right:default;' +
+        'hair:default;' +
+        'head:default;' +
+        'legs:default;' +
+        'eyes:default;' +
+        'mouth:default;' +
+        'tits:default;' +
+        'torso:default;',
 
-  'luna': 'backdrops:bathroom;' +
-    'shirts:tits.pressed/vest_r;' +
-    'skirts:high_long;' +
-    'arms_right:default;' +
-    'arms_left:default;' +
-    'cheeks:default;' +
-    'eyes:blue_default;' +
-    'hair:wanded;' +
-    'head:default;' +
-    'legs:default;' +
-    'mouth:nude_default;' +
-    'pubic_hair:default;' +
-    'tits:default;' +
-    'torso:default;' +
-    'genitalia:default;',
+    'luna': 'backdrops:bathroom;' +
+        'shirts:tits.pressed/vest_r;' +
+        'skirts:high_long;' +
+        'arms_right:default;' +
+        'arms_left:default;' +
+        'cheeks:default;' +
+        'eyes:blue_default;' +
+        'hair:wanded;' +
+        'head:default;' +
+        'legs:default;' +
+        'mouth:nude_default;' +
+        'pubic_hair:default;' +
+        'tits:default;' +
+        'torso:default;' +
+        'genitalia:default;',
 
-  'susan': 'backdrops:bathroom;' +
-    'shirts:tits.pressed/vest_h;' +
-    'skirts:high_long;' +
-    'arms_left:default;' +
-    'arms_right:default;' +
-    'eyes:green_default;' +
-    'genitalia:default;' +
-    'hair:default;' +
-    'head:default;' +
-    'legs:default;' +
-    'mouth:nude_default;' +
-    'tits:pressed;' +
-    'torso:default;',
+    'susan': 'backdrops:bathroom;' +
+        'shirts:tits.pressed/vest_h;' +
+        'skirts:high_long;' +
+        'arms_left:default;' +
+        'arms_right:default;' +
+        'eyes:green_default;' +
+        'genitalia:default;' +
+        'hair:default;' +
+        'head:default;' +
+        'legs:default;' +
+        'mouth:nude_default;' +
+        'tits:pressed;' +
+        'torso:default;',
 
-  'astoria': 'backdrops:bathroom;' +
-    'arms_left:l_arm_hips;' +
-    'arms_right:r_arm_hips;' +
-    'torso:base_01;' +
-    'head:pigtails_blonde;' +
-    'tits:base;' +
-    'hair:stephanie_base;' +
-    'torso:base_01;',
+    'astoria': 'backdrops:bathroom;' +
+        'arms_left:l_arm_hips;' +
+        'arms_right:r_arm_hips;' +
+        'torso:base_01;' +
+        'head:pigtails_blonde;' +
+        'tits:base;' +
+        'hair:stephanie_base;' +
+        'torso:base_01;',
 
-  'tonks': 'backdrops:bathroom;',
-  'snape': 'backdrops:bathroom;',
-  'voldemort': 'backdrops:bathroom;',
+    'tonks': 'backdrops:bathroom;',
+    'snape': 'backdrops:bathroom;',
+    'voldemort': 'backdrops:bathroom;',
 };
